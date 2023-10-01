0dfb67ef21a3b85eb218d378473455727b084d32
Pramath Bhat <98164339+pramath12@users.noreply.github.com>
2023-10-01 13:01:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("input-form");
+    const resultsDiv = document.getElementById("results");
+
+    form.addEventListener("submit", async function (event) {
+        event.preventDefault();
+
+        const templateDescInput = document.getElementById("template-desc").value;
+        const compactBufferInput = document.getElementById("compact-buffer").value;
+
+        // Perform AJAX request to backend
+        const response = await fetch("http://127.0.0.1:5000/parse", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin':'*'
+            },
+            body: JSON.stringify({
+                templateDesc: templateDescInput,
+                compactBuffer: compactBufferInput,
+            }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();      
+      
+            resultsDiv.appendChild(createTable(data));
+            // Display results
+            //resultsDiv.innerHTML = `<pre>${JSON.stringify(data, null, 1)}</pre>`;
+        } else {
+            resultsDiv.innerHTML = "<p>Error while parsing.</p>";
+        }
+    });
+});
+
+function createTable(data) {
+    const table = document.createElement("table");
+    table.classList.add("result-table");
+
+    // Create a heading row
+    const headingRow = table.insertRow();
+    const serialNoCell = headingRow.insertCell(0);
+    const dataTypeCell = headingRow.insertCell(1);
+    const bufferCell = headingRow.insertCell(2);
+
+    // Set heading text
+    serialNoCell.textContent = "Serial No.";
+    dataTypeCell.textContent = "Data Type";
+    bufferCell.textContent = "Buffer";
+
+    for (const key in data) {
+        const value = data[key];
+
+        const row = table.insertRow();
+        const serialNoCell = row.insertCell(0);
+        const dataTypeCell = row.insertCell(1);
+        const bufferCell = row.insertCell(2);
+
+        serialNoCell.textContent = key;
+        dataTypeCell.textContent = JSON.stringify(value[0], null, 1);
+        bufferCell.textContent = JSON.stringify(value[1], null, 1);
+    }
+
+    return table;
+}
